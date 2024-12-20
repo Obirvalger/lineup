d31ce25ecaafa67478b73abd00b0a97a70811d7b
Mikhail Gordeev <obirvalger@altlinux.org>
2024-12-20 19:32:19 +0300

    Add items variable type

--- src/items.rs
+++ src/items.rs
@@ -1,7 +1,9 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use cmd_lib::run_fun;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
+use crate::error::Error;
 use crate::render::Render;
 use crate::string_or_int::StringOrInt;
 use crate::template::Context;
@@ -33,11 +35,19 @@ pub struct ItemsCommand {
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "kebab-case")]
+pub struct ItemsVariable {
+    #[serde(alias = "var")]
+    pub variable: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
 #[serde(untagged)]
 pub enum Items {
     Words(Vec<StringOrInt>),
     Seq(ItemsSeq),
     Command(ItemsCommand),
+    Variable(ItemsVariable),
 }
 
 impl Items {
@@ -54,6 +64,30 @@ impl Items {
                 let cmd = command.command.render(context, "list items command")?;
                 let out = run_fun!(sh -c $cmd)?;
                 out.lines().map(|l| l.to_string()).collect::<Vec<String>>()
+            }
+            Items::Variable(variable) => {
+                let var_name = variable.variable.render(context, "list items variable")?;
+                let var = context
+                    .get(&var_name)
+                    .ok_or_else(|| Error::NoItemsVar(var_name.to_string()))?;
+                match var {
+                    Value::Array(a) => {
+                        let mut items = Vec::with_capacity(a.len());
+                        for item in a {
+                            let item = match item {
+                                Value::Bool(b) => b.to_string(),
+                                Value::Null => "".to_string(),
+                                Value::Number(n) => n.to_string(),
+                                Value::String(s) => s.to_string(),
+                                _ => bail!(Error::WrongItemsVarType(var_name)),
+                            };
+                            items.push(item);
+                        }
+                        items
+                    }
+                    Value::Object(o) => o.keys().map(|k| k.to_string()).collect(),
+                    _ => bail!(Error::WrongItemsVarType(var_name)),
+                }
             }
         };
 
